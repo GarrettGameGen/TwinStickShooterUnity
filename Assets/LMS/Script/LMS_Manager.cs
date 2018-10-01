3d344b6a817b06f1c34ac511891a7efd3dd8379c
Garrett <garrett@gamegen.com>
2018-09-30 17:59:00 -0700

    LMS Class fcukery

--- Assets/LMS/Script/LMS_Manager.cs
+++ Assets/LMS/Script/LMS_Manager.cs
@@ -2,11 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LMS_Manager : MonoBehaviour {
 
+public class LMS_Manager : MonoBehaviour {
+    public class Lesson {
+        public string LessonName;
+        //public Lesson childLesson;
+    };
+    
+    public string FunnyName;
+    public Lesson[] lesson = new Lesson[2];
+    
 	// Use this for initialization
 	void Start () {
-		
+
 	}
 	
 	// Update is called once per frame
